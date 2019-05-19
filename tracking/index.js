9e5e908c2f33d98a1b0faa6a406286845fe355e5
mrowl <wastedowl777@gmail.com>
2019-05-19 13:58:56 +0300

    init

--- tracking/index.js
+++ tracking/index.js
@@ -1,17 +1,24 @@
-
-var search_button = $('.icon-search'),
+function init(){
+  var search_button = $('.icon-search'),
     close_button  = $('.icon-times'),
     input = $('.input');
-    console.log(search_button);
-search_button.on('click',function(){
-  console.log('clc');
-  $(this).parent().addClass('open');
-  close_button.fadeIn(500);
-  input.fadeIn(500);
-});
+  console.log(search_button);
+  search_button.on('click',function(){
+    console.log('clc');
+    $(this).parent().addClass('open');
+    close_button.fadeIn(500);
+    input.fadeIn(500);
+  });
 
-close_button.on('click',function(){
-  search_button.parent().removeClass('open');
-  close_button.fadeOut(500);
-  input.fadeOut(500);
+  close_button.on('click',function(){
+    search_button.parent().removeClass('open');
+    close_button.fadeOut(500);
+    input.fadeOut(500);
+  });
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    init();
 });
