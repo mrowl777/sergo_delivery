df7a79f57dfa7e67c3f7c77b1deab2cdc64cbde9
mrowl <wastedowl777@gmail.com>
2019-05-19 13:55:25 +0300

    console

--- tracking/index.js
+++ tracking/index.js
@@ -3,13 +3,14 @@ var search_button = $('.icon-search'),
     close_button  = $('.icon-times'),
     input = $('.input');
 search_button.on('click',function(){
-  $(this).parent().parent().addClass('open');
+  console.log('clc');
+  $(this).parent().addClass('open');
   close_button.fadeIn(500);
   input.fadeIn(500);
 });
 
 close_button.on('click',function(){
-  search_button.parent().parent().removeClass('open');
+  search_button.parent().removeClass('open');
   close_button.fadeOut(500);
   input.fadeOut(500);
 });
