5a1c34284de4ccfd1b2aad8b71c856e62ba1ef34
mrowl <wastedowl777@gmail.com>
2019-05-21 23:53:23 +0300

    optionid

--- admin/index.js
+++ admin/index.js
@@ -1,5 +1,11 @@
 function init(){
-   
+    $('select').change(function() {
+        var option = $(this).find('option:selected');
+        var value = option.val();
+        var id = $(this).parent().parent().attr('id');
+        console.log(value);
+        console.log(id);
+    });
   }
   
   document.addEventListener('DOMContentLoaded', function () {
